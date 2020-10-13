dd5abea6e8b54a58497c9d617cb0408dee07f066
Rebecca Howard <howardrebecca@gmail.com>
2020-10-13 11:42:44 -0700

    reformatted code

--- code/Access_import.R
+++ code/Access_import.R
@@ -1,24 +1,30 @@
-setwd('/Users/howar/Documents/Oregon State/ORnearshore_groundfish')
+setwd('/Users/howar/Documents/Oregon State/ORnearshore_groundfish/code/')
 
 # requires 32-bit R, cannot use 64-bit
-library(RODBC)
-con <- odbcConnectAccess2007("C:\\Users\\howar\\Documents\\Oregon State\\ORnearshore_groundfish\\data\\ODFW_data\\CiannelliDataRequest.accdb")
+
+con <- odbcConnectAccess2007("../data/ODFW_data/CiannelliDataRequest.accdb")
 sqlTables(con)$BottomTrawlTickets
+sqlTables(con)$VesselData
 
 fish_tickets <- sqlFetch(con, "BottomTrawlTickets")
 str(fish_tickets)
 save(fish_tickets, file = "fish_tickets")
-write.csv(fish_tickets,"C:\\Users\\howar\\Documents\\Oregon State\\ORnearshore_groundfish\\data\\ODFW_data\\fish_tickets.csv",row.names=FALSE)
+write.csv(fish_tickets,
+  "../data/ODFW_data/fish_tickets.csv",
+  row.names = FALSE)
 
 logbooks <- sqlFetch(con, "BottomTrawlLogs")
 str(logbooks)
 save(logbooks, file = "logbooks")
-write.csv(logbooks,"C:\\Users\\howar\\Documents\\Oregon State\\ORnearshore_groundfish\\data\\ODFW_data\\logbooks.csv",row.names=FALSE)
+write.csv(logbooks,
+  "../data/ODFW_data/logbooks.csv",
+  row.names = FALSE)
 
-sqlTables(con)$VesselData
 vessel_data <- sqlFetch(con, "VesselData")
 str(vessel_data)
 save(vessel_data, file = "vessel_data")
-write.csv(vessel_data,"C:\\Users\\howar\\Documents\\Oregon State\\ORnearshore_groundfish\\data\\ODFW_data\\vessel_data.csv",row.names=FALSE)
+write.csv(vessel_data,
+  "../data/ODFW_data/vessel_data.csv",
+  row.names = FALSE)
 
 odbcCloseAll()
