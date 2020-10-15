acc20033b73645a32d5f9a897f639cc6daa5f2cd
Rebecca Howard <howardrebecca@gmail.com>
2020-10-15 12:48:30 -0700

    Reformat code

--- code/functions/starry_function.R
+++ code/functions/starry_function.R
@@ -1,22 +1,39 @@
-# for starry flounder only
-species_plot <- function(lower_yr, upper_yr){
-  plot(1,1,xlim=range(subset_starry$lon,na.rm=TRUE)+c(-.5,.2),
-       ylim=range(subset_starry$lat,na.rm=TRUE)+c(-.2,.2),
-       ylab=expression(paste("latitude ("^0,'N)')),
-       xlab=expression(paste("longitude ("^0,'W)')),
-       main=paste('Starry Flounder ', lower_yr, 's'))
-  map("worldHires",fill=T,col="grey",add=T)
-  points(subset_starry$lon[subset_starry$year>=lower_yr & subset_starry$year<=upper_yr & subset_starry$pres==1],
-         subset_starry$lat[subset_starry$year>=lower_yr & subset_starry$year<=upper_yr & subset_starry$pres==1],
-         pch=".",col='purple')
-  contour(unique(bathy.dat$lon),sort(unique(bathy.dat$lat)),bathy.mat,levels=c(-10,-200),
-          labcex=0.7,add=T,col='black', labels = NULL, lwd = 2)
+# Single Species
+species_plot <- function(lower_yr, upper_yr) {
+  plot(1, 1, xlim = range(species_subset$lon, na.rm = TRUE) + c(-.5, .2),
+       ylim = range(species_subset$lat, na.rm = TRUE) + c(-.2, .2),
+       ylab = "latitude °N",
+       xlab = "longitude °W",
+       main = paste(lower_yr, 's', sep = ""))
+  map("worldHires",
+      fill = T,
+      col = "grey",
+      add = T)
+  points(species_subset$lon[species_subset$year >= lower_yr &
+                              species_subset$year <= upper_yr &
+                              species_subset$pres == 1],
+         species_subset$lat[species_subset$year >= lower_yr &
+                              species_subset$year <= upper_yr &
+                              species_subset$pres == 1],
+         pch = ".",
+         col = 'purple')
+  contour(unique(bathy.dat$lon),
+          sort(unique(bathy.dat$lat)),
+          bathy.mat,
+          levels = c(-10, -200),
+          labcex = 0.7,
+          add = T,
+          col = 'black',
+          labels = NULL,
+          lwd = 2)
 }
 
+# Plot the maps to check
+#windows(width = 28, height = 18)
+#par(mfrow = c(1, 4))
+#species_plot(1980, 1989)
+#species_plot(1990, 1999)
+#species_plot(2000, 2009)
+#species_plot(2010, 2017)
 
-
-windows(width=28,height=18)
-par(mfrow=c(1,4))
-
-grid_plot(1980, 1989)
-grid_plot(1990, 1999)
+#rm(list = ls())
