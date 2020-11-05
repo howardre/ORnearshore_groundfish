a9fd39d00bb5c7fdda5f8ab21b55c7c1d0f52942
Rebecca Howard <howardrebecca@gmail.com>
2020-11-05 11:28:31 -0800

    Edit TGAM function to fix bugs, add rex and dover functions
    - Changed the list to data frame
    - Added year as a required variable
    - Reformatting
    - New functions for models w/o year
    - New function to plot AIC

--- code/functions/TGAM_selection.R
+++ code/functions/TGAM_selection.R
@@ -1,9 +1,41 @@
-# SHOUld work for fishes with year in the gam. Couldn't figure out indexing for line
-# 23 though
 # Go through each year to find the threshold year for sample ----
-get_tgam_yr_aic <- function(df, yr) {
+get_yr_aic <- function(df, yr) {
   df$thr <- ifelse(df$year <= yr, 'before', 'after')
   aic_year <-  gam(pres ~ factor(year) +
+                     s(longitude, latitude, by = factor(thr)) +
+                     s(julian),
+                   data = df,
+                   family = binomial)$aic
+  return_list <- data.frame(aic_year, yr)
+}
+
+# Returns AIC difference, best year, best TGAM and reference GAM ----
+get_tgam <- function(df, years) {
+  ref_gam <- gam(pres ~ factor(year) +
+                   s(julian) +
+                   s(longitude, latitude),
+                 data = df,
+                 family = binomial)
+  all_tgams <- data.frame(aic_year = rep(0, length(years)),
+                          year = rep(0, length(years)))
+  all_tgams <- map(years, ~ get_yr_aic(df, .x))
+  best_tgam <- all_tgams[[which.min(sapply(1:length(all_tgams),
+                                           function(x) min(all_tgams[[x]]$aic_year)))]]
+  diff <- ref_gam$aic - best_tgam$aic_year
+  df$thr <- ifelse(df$year <= best_tgam$yr, 'before', 'after')
+  final_tgam <-  gam(pres ~ factor(year) +
+                          s(longitude, latitude, by = factor(thr)) +
+                          s(julian),
+                        data = df,
+                        family = binomial)
+  all_aic <- sapply(all_tgams, function(x) {as.numeric(x[1])})
+  return_list <- list(ref_gam, final_tgam, best_tgam$yr, diff, all_aic)
+}
+
+# Get
+get_aic_woyear <- function(df, yr) {
+  df$thr <- ifelse(df$year <= yr, 'before', 'after')
+  aic_year <-  gam(pres ~ factor(thr) +
                      s(longitude, latitude, by = factor(thr)) +
                      s(julian),
                    data = df,
@@ -12,23 +44,38 @@ get_tgam_yr_aic <- function(df, yr) {
 }
 
 # Returns AIC difference, best year, best TGAM and Reference GAM ----
-get_tgam <- function(df) {
-  ref_gam <- gam(pres ~ factor(year) +
-                   s(julian) +
+get_tgam_woyear <- function(df) {
+  ref_gam <- gam(pres ~ s(julian) +
                    s(longitude, latitude),
-                 data = samp,
+                 data = df,
                  family = binomial)
   all_tgams <- data.frame(aic_year = rep(0, length(years)),
                           year = rep(0, length(years)))
-  all_tgams <- map(years, ~ get_tgam_yr_aic(samp, .x))
-  best_tgam <- all_tgams[[which.min(sapply(1:length(all_tgams),function(x)min(all_tgams[[x]]$aic_year)))]]
+  all_tgams <- map(years, ~ get_aic_woyear(df, .x))
+  best_tgam <- all_tgams[[which.min(sapply(1:length(all_tgams),
+                                           function(x) min(all_tgams[[x]]$aic_year)))]]
   diff <- ref_gam$aic - best_tgam$aic_year
-  # now get actual tgam
   df$thr <- ifelse(df$year <= best_tgam$yr, 'before', 'after')
-  final_tgam <-  gam(pres ~ factor(year) +
-                          s(longitude, latitude, by = factor(thr)) +
-                          s(julian),
-                        data = df,
-                        family = binomial)
-  return_list <- list(ref_gam, final_tgam, best_tgam$yr, diff)
+  final_tgam <-  gam(pres ~ factor(thr) +
+                       s(longitude, latitude, by = factor(thr)) +
+                       s(julian),
+                     data = df,
+                     family = binomial)
+  all_aic <- sapply(all_tgams, function(x) {as.numeric(x[1])})
+  return_list <- list(ref_gam, final_tgam, best_tgam$yr, diff, all_aic)
+}
+
+# Use to plot the final results
+plot_AIC <- function(tgam, years) {
+  plot(years,
+       tgam[[5]], # need y axis to be the AIC range for all GAMs
+       type = 'b',
+       xlab = 'Year',
+       ylab = 'AIC',
+       main = deparse(substitute(tgam)),
+       cex.main = 1.4,
+       cex.lab = 1.2,
+       cex.axis = 1.2)
+  abline(v = tgam[[3]], lty = 2)
+  abline(h = AIC(tgam[[1]]), lty = 2)
 }
