1f05e737aaf445c3f9e64dc1311c001f375607d8
Rebecca Howard <howardrebecca@gmail.com>
2020-11-05 11:26:40 -0800

    Add threshold code and change plotting code

--- code/TGAM_analysis.R
+++ code/TGAM_analysis.R
@@ -15,9 +15,10 @@ trawl_data <- read.delim("../data/NMFS_data/trawl_data.txt", header = T)
 OR_fish <- read.delim("../data/NMFS_data/OR_fish.txt", header = T)
 load("../data/bathy.dat")
 load("../data/bathy.mat")
-source("functions/distance.function.R")
-source("functions/vis.gam_COLORS.R")
+source("functions/distance_function.R")
+source("functions/vis_gam_COLORS.R")
 source("functions/subset_species.R")
+source("functions/TGAM_selection.R")
 jet_colors<-colorRampPalette(c("#F7FCFD", "#E0ECF4", "#BFD3E6", "#9EBCDA", "#8C96C6", "#8C6BB1", "#88419D", "#6E016B"))
 
 # Subset the data to contain only species of interest ----
@@ -30,3 +31,47 @@ rex_subset <- subset_species("Glyptocephalus zachirus", OR_fish, trawl_data)
 lingcod_subset <- subset_species("Ophiodon elongatus", OR_fish, trawl_data)
 petrale_subset <- subset_species("Eopsetta jordani", OR_fish, trawl_data)
 sablefish_subset <- subset_species("Anoplopoma fimbria", OR_fish, trawl_data)
+
+# Find thresholds for all 8 species
+years <- sort(unique(arrowtooth_subset$year))[4:22]
+arrowtooth_tgam <- get_tgam(arrowtooth_subset, years)
+english_tgam <- get_tgam(english_subset, years)
+sanddab_tgam <- get_tgam(sanddab_subset, years)
+dover_tgam <- get_tgam_woyear(dover_subset, years)
+rex_tgam <- get_tgam_woyear(rex_subset, years)
+lingcod_tgam <- get_tgam(lingcod_subset, years)
+petrale_tgam <- get_tgam(petrale_subset, years)
+sablefish_tgam <- get_tgam(sablefish_subset, years)
+
+# Plot AIC for all species
+windows()
+par(family = "serif")
+plot(
+  years,
+  arrowtooth_tgam[[5]], # need y axis to be the AIC range for all GAMs
+  type = 'b',
+  xlab = 'Year',
+  ylab = 'AIC',
+  main = "Arrowtooth Flounder",
+  cex.main = 1.4,
+  cex.lab = 1.2,
+  cex.axis = 1.2
+)
+abline(v = arrowtooth_tgam[[3]], lty = 2)
+text(2003, 3465, 'Before', cex = 1.2)
+text(2011, 3465, 'After', cex = 1.2)
+abline(h = AIC(arrowtooth_tgam[[1]]), lty = 2)
+
+plot_AIC <- function(tgam, years) {
+  plot(years,
+       tgam[[5]], # need y axis to be the AIC range for all GAMs
+       type = 'b',
+       xlab = 'Year',
+       ylab = 'AIC',
+       main = deparse(substitute(tgam)),
+       cex.main = 1.4,
+       cex.lab = 1.2,
+       cex.axis = 1.2)
+  abline(v = tgam[[3]], lty = 2)
+  abline(h = AIC(tgam[[1]]), lty = 2)
+}
