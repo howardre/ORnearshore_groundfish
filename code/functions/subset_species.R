c5451055a8079ec36b62733f726798ba8cbc904f
Rebecca Howard <howardrebecca@gmail.com>
2020-11-06 12:52:38 -0800

    Edit columns for selection

--- code/functions/subset_species.R
+++ code/functions/subset_species.R
@@ -3,7 +3,7 @@ subset_species <- function(species, catch, tows){
   match_id <- match(tows$trawl_id, OR_subset$trawl_id)
   tows$lncpue <- OR_subset$lncpue_n[match_id]
   tows$lncpue[is.na(tows$lncpue)] <- 0
-  selected_species <- select(tows, julian, year, lncpue, latitude, longitude)
+  selected_species <- select(tows, julian, year, lncpue, latitude, longitude, PDO, NPGO, bottom_temp, depth_m)
   selected_species <- na.omit(selected_species)
   selected_species$pres <- 1 * (selected_species$lncpue > 0)
   return(selected_species)
