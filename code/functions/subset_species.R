676eb8df7ec10f35f70a10fe601ab8ab1021c049
Rebecca Howard <howardrebecca@gmail.com>
2020-11-06 19:00:36 -0800

    Add new function that cuts out temp outliers

--- code/functions/subset_species.R
+++ code/functions/subset_species.R
@@ -8,3 +8,15 @@ subset_species <- function(species, catch, tows){
   selected_species$pres <- 1 * (selected_species$lncpue > 0)
   return(selected_species)
 }
+
+subset_species_temp <- function(species, catch, tows){
+  OR_subset <- catch[catch$scientific_name == species, ]
+  match_id <- match(tows$trawl_id, OR_subset$trawl_id)
+  tows$lncpue <- OR_subset$lncpue_n[match_id]
+  tows$lncpue[is.na(tows$lncpue)] <- 0
+  selected_species <- select(tows, julian, year, lncpue, latitude, longitude, PDO, NPGO, bottom_temp, depth_m)
+  selected_species <- na.omit(selected_species)
+  selected_species$pres <- 1 * (selected_species$lncpue > 0)
+  selected_species <- selected_species[selected_species$bottom_temp < 11,] # Filter out outlier temperature tows
+  return(selected_species)
+}
