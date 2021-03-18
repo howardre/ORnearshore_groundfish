0a22e2e607a2b64d61b10d3d81ce68dc554a1f55
Rebecca Howard <howardrebecca@gmail.com>
2021-03-18 16:48:07 -0700

    Modify to make count variable for zip

--- code/functions/subset_species.R
+++ code/functions/subset_species.R
@@ -3,7 +3,8 @@ subset_species <- function(species, catch, tows){
   match_id <- match(tows$trawl_id, OR_subset$trawl_id)
   tows$lncpue <- OR_subset$lncpue_n[match_id]
   tows$lncpue[is.na(tows$lncpue)] <- 0
-  selected_species <- select(tows, julian, year, lncpue, latitude, longitude, PDO, NPGO, bottom_temp, depth_m)
+  selected_species <- select(tows, julian, year, lncpue, latitude, longitude, PDO, NPGO,
+                             bottom_temp, depth_m, program)
   selected_species <- na.omit(selected_species)
   selected_species$pres <- 1 * (selected_species$lncpue > 0)
   return(selected_species)
@@ -14,7 +15,9 @@ subset_species_temp <- function(species, catch, tows){
   match_id <- match(tows$trawl_id, OR_subset$trawl_id)
   tows$lncpue <- OR_subset$lncpue_n[match_id]
   tows$lncpue[is.na(tows$lncpue)] <- 0
-  selected_species <- select(tows, julian, year, lncpue, latitude, longitude, PDO, NPGO, bottom_temp, depth_m)
+  tows$count <- as.integer(tows$lncpue)
+  selected_species <- select(tows, julian, year, lncpue, latitude, longitude, PDO, NPGO,
+                             bottom_temp, depth_m, count, program)
   selected_species <- na.omit(selected_species)
   selected_species$pres <- 1 * (selected_species$lncpue > 0)
   selected_species <- selected_species[selected_species$bottom_temp < 11,] # Filter out outlier temperature tows
