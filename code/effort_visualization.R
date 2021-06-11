94a8edd493c6e52dc606b3befa12f4a36f315bbf
Rebecca Howard <howardrebecca@gmail.com>
2021-06-11 14:08:17 -0700

    Remove code and swap for functions

--- code/effort_visualization.R
+++ code/effort_visualization.R
@@ -6,6 +6,7 @@
 setwd("C:/Users/howar/Documents/Oregon State/ORnearshore_groundfish/code/")
 load("../data/ODFW_data/logbooks_corrected")
 source("functions/grid_fill.R")
+source("functions/grid_data.R")
 library(sgeostat)
 library(maps)
 library(mapdata)
@@ -34,6 +35,8 @@ filtered$hauls <- filtered$GEAR[filtered$GEAR > 0] <- 1
 trawl_counts <- filtered[filtered$species == 'PTRL_ADJ', ] # use one species since just counting effort
 
 ## Make a regular grid and count stations within each grid cell ----
+# These demonstrate the filling of grid cells
+# grid_fill just gives the plot, grid_data gives the matrix and plot
 # All years
 years_data <- length(unique(trawl_counts$year))
 zlat <- (latd[1:(length(latd) - 1)] + latd[2:length(latd)]) / 2
@@ -81,252 +84,20 @@ plot(trawl_counts$lon,
 grid_fill(30, 25, 2009, 2019, trawl_counts)
 
 ## decades
-zlat <- (latd[1:(length(latd) - 1)] + latd[2:length(latd)]) / 2
-zlon <- (lond[1:(length(lond) - 1)] + lond[2:length(lond)]) / 2
-eighties.data <- length(unique(trawl_counts$year[trawl_counts$year > 1980 &
-                                trawl_counts$year < 1990]))
-z_matrix1 <- matrix(nstations1,
-                    ncol = length(zlat),
-                    nrow = length(zlon),
-                    byrow = F) / eighties.data
-z_matrix1 <- ifelse(z_matrix1 == 0, NA, z_matrix1)
-nineties.data <- length(unique(trawl_counts$year[trawl_counts$year > 1989 &
-                                trawl_counts$year < 2000]))
-z_matrix2 <- matrix(nstations2,
-                    ncol = length(zlat),
-                    nrow = length(zlon),
-                    byrow = F) / nineties.data
-z_matrix2 <- ifelse(z_matrix2 == 0, NA, z_matrix2)
-thousands.data <- length(unique(trawl_counts$year[trawl_counts$year > 1999 &
-                                trawl_counts$year < 2010]))
-z_matrix3 <- matrix(nstations3,
-                    ncol = length(zlat),
-                    nrow = length(zlon),
-                    byrow = F) / thousands.data
-z_matrix3 <- ifelse(z_matrix3 == 0, NA, z_matrix3)
-tens.data <- length(unique(trawl_counts$year[trawl_counts$year > 2009 &
-                                trawl_counts$year < 2018]))
-z_matrix4 <- matrix(nstations4,
-                    ncol = length(zlat),
-                    nrow = length(zlon),
-                    byrow = F) / tens.data
-z_matrix4 <- ifelse(z_matrix4 == 0, NA, z_matrix4)
+eighties_data <- grid_data(30, 25, 1980, 1990, trawl_counts)
+nineties_data <- grid_data(30, 25, 1989, 2000, trawl_counts)
+thousands_data <- grid_data(30, 25, 1999, 2010, trawl_counts)
+teens_data <- grid_data(30, 25, 2009, 2018, trawl_counts)
 
 ## Map the grids ----
-# windows(width=11,height=5)
-# par(mfrow=c(1,2))
 windows(width = 15, height = 9)
 par(mfrow = c(1, 4),
     family = 'serif',
     mar = c(4, 5, 3, .3) + .1)
-
-image(
-  zlon,
-  zlat,
-  z_matrix1,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  xlim = c(-125, -123.6),
-  ylim = c(42, 47),
-  main = "Fishing Effort 1980s",
-  ylab = expression(paste("Latitude (" ^ 0, 'N)')),
-  xlab = expression(paste("Longitude (" ^ 0, 'W)')),
-  zlim = c(0, 230)
-)
-map("worldHires",
-    fill = T,
-    col = "grey",
-    add = T)
-image.plot(
-  legend.only = T,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  legend.shrink = 0.2,
-  smallplot = c(.76, .81, .09, .25),
-  legend.cex = 0.7,
-  axis.args = list(cex.axis = 0.9),
-  legend.width = 1,
-  zlim = c(0, 230),
-  legend.lab = "avg. number of tows"
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -100,
-  col = "gray19",
-  add = T
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -200,
-  col = "gray19",
-  add = T
-)
-points(-124.0535, 44.6368, pch = 20)
-text(-124.0535, 44.6368, "Newport", adj = c(0, 1.2))
-points(-123.8313, 46.1879, pch = 20)
-text(-123.88028, 46.13361, "Astoria", adj = c(0, 1.2))
-points(-124.3, 43.3, pch = 20)
-text(-124.3, 43.3, "Charleston", adj = c(0, 1.2))
-
-image(
-  zlon,
-  zlat,
-  z_matrix2,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  xlim = c(-125, -123.6),
-  ylim = c(42, 47),
-  main = "Fishing Effort 1990s",
-  ylab = expression(paste("Latitude (" ^ 0, 'N)')),
-  xlab = expression(paste("Longitude (" ^ 0, 'W)')),
-  zlim = c(0, 230)
-)
-map("worldHires",
-    fill = T,
-    col = "grey",
-    add = T)
-image.plot(
-  legend.only = T,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  legend.shrink = 0.2,
-  smallplot = c(.76, .81, .09, .25),
-  legend.cex = 0.7,
-  axis.args = list(cex.axis = 0.9),
-  legend.width = 1,
-  zlim = c(0, 230),
-  legend.lab = "avg. number of tows"
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -100,
-  col = "gray19",
-  add = T
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -200,
-  col = "gray19",
-  add = T
-)
-points(-124.0535, 44.6368, pch = 20)
-text(-124.0535, 44.6368, "Newport", adj = c(0, 1.2))
-points(-123.8313, 46.1879, pch = 20)
-text(-123.88028, 46.13361, "Astoria", adj = c(0, 1.2))
-points(-124.3, 43.3, pch = 20)
-text(-124.3, 43.3, "Charleston", adj = c(0, 1.2))
-
-image(
-  zlon,
-  zlat,
-  z_matrix3,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  xlim = c(-125, -123.6),
-  ylim = c(42, 47),
-  main = "Fishing Effort 2000s",
-  ylab = expression(paste("Latitude (" ^ 0, 'N)')),
-  xlab = expression(paste("Longitude (" ^ 0, 'W)')),
-  zlim = c(0, 230)
-)
-map("worldHires",
-    fill = T,
-    col = "grey",
-    add = T)
-image.plot(
-  legend.only = T,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  legend.shrink = 0.2,
-  smallplot = c(.76, .81, .09, .25),
-  legend.cex = 0.7,
-  axis.args = list(cex.axis = 0.9),
-  legend.width = 1,
-  zlim = c(0, 230),
-  legend.lab = "avg. number of tows"
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -100,
-  col = "gray19",
-  add = T
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -200,
-  col = "gray19",
-  add = T
-)
-points(-124.0535, 44.6368, pch = 20)
-text(-124.0535, 44.6368, "Newport", adj = c(0, 1.2))
-points(-123.8313, 46.1879, pch = 20)
-text(-123.88028, 46.13361, "Astoria", adj = c(0, 1.2))
-points(-124.3, 43.3, pch = 20)
-text(-124.3, 43.3, "Charleston", adj = c(0, 1.2))
-
-image(
-  zlon,
-  zlat,
-  z_matrix4,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  xlim = c(-125, -123.6),
-  ylim = c(42, 47),
-  main = "Fishing Effort 2010s",
-  ylab = expression(paste("Latitude (" ^ 0, 'N)')),
-  xlab = expression(paste("Longitude (" ^ 0, 'W)')),
-  zlim = c(0, 230)
-)
-map("worldHires",
-    fill = T,
-    col = "grey",
-    add = T)
-image.plot(
-  legend.only = T,
-  col = hcl.colors(40, "RdYlBu", rev = T),
-  legend.shrink = 0.2,
-  smallplot = c(.76, .81, .09, .25),
-  legend.cex = 0.7,
-  axis.args = list(cex.axis = 0.9),
-  legend.width = 1,
-  zlim = c(0, 230),
-  legend.lab = "avg. number of tows"
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -100,
-  col = "gray19",
-  add = T
-)
-contour(
-  unique(bathy_dat$lon),
-  sort(unique(bathy_dat$lat)),
-  bathy_mat,
-  lwd = 1,
-  levels = -200,
-  col = "gray19",
-  add = T
-)
-points(-124.0535, 44.6368, pch = 20)
-text(-124.0535, 44.6368, "Newport", adj = c(0, 1.2))
-points(-123.8313, 46.1879, pch = 20)
-text(-123.88028, 46.13361, "Astoria", adj = c(0, 1.2))
-points(-124.3, 43.3, pch = 20)
-text(-124.3, 43.3, "Charleston", adj = c(0, 1.2))
+grid_map(30, 25, 1980, 1990, trawl_counts, eighties_data, "Fishing Effort 1980s", bathy_dat, bathy_mat)
+grid_map(30, 25, 1989, 2000, trawl_counts, nineties_data, "Fishing Effort 1990s", bathy_dat, bathy_mat)
+grid_map(30, 25, 1999, 2010, trawl_counts, thousands_data, "Fishing Effort 2000s", bathy_dat, bathy_mat)
+grid_map(30, 25, 2009, 2018, trawl_counts, teens_data, "Fishing Effort 2010s", bathy_dat, bathy_mat)
 
 ## Survey
 load('/Users/howar/Documents/Oregon State/Thesis/Data Visualization/trawl_data')
