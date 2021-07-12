442fef7f9793a424ecff8ca728b4727ed0697068
Rebecca Howard <howardrebecca@gmail.com>
2021-07-12 14:13:17 -0700

    Edit contour function to specify bins

--- code/functions/depth_contours.R
+++ code/functions/depth_contours.R
@@ -3,7 +3,10 @@
 depth_contours <- function(subset, year_upper, color, title){
   subset %>% filter(year <= year_upper) %>% filter(CPUE > 0) %>%
     ggplot(aes(x = depth, y = lat)) +
-    stat_density_2d(aes(fill = stat(level)), geom = "polygon", show.legend = F) +
+    stat_density_2d(aes(fill = stat(level)),
+                    geom = "polygon",
+                    show.legend = F,
+                    bins = 12) +
     scale_fill_distiller(palette = color, direction = -1) +
     scale_x_continuous(expand = c(0, 0), limits = c(-200, 0)) +
     scale_y_continuous(expand = c(0, 0), limits = c(40, 49)) +
